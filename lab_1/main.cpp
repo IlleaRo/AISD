e7cf7187ac797fd16b0aa5848f9da35be324781e
FeckingPotato <cornilow.serega@gmail.com>
2024-02-18 15:05:47 +0700

    заготовки тестов

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int tests()
+{
+    int function_counter = 0;
+    /*
+     * if (test_function() != success)
+     * {
+     *     return function_counter;
+     * }
+     * function_counter++;
+     */
+    return 0;
+}
+
 int main()
 {
+    int test_result;
+    cout << "Running tests" << endl;
+    test_result = tests();
+    if (test_result != 0)
+    {
+        cout << "Tests failed at function #" << test_result << endl;
+    }
+    else
+    {
+        cout << "Success" << endl;
+    }
     return 0;
 }
