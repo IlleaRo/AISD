b0554a22a23609c2363f78468b6c8fed986d74c5
FeckingPotato <62855317+FeckingPotato@users.noreply.github.com>
2024-02-20 22:31:31 +0700

    реализовал всё кроме удаления

--- lab_1/list.cpp
+++ lab_1/list.cpp
@@ -1,4 +1,5 @@
 #include "list.h"
+#include <stdexcept>
 
 template<class T>
 list<T>::node::node(T item)
@@ -9,15 +10,13 @@ list<T>::node::node(T item)
 template<class T>
 T list<T>::iterator::operator*()
 {
-    // TODO: IMPLEMENT ME
-    return nullptr;
+    return this->cur_node->item;
 }
 
 template<class T>
-list<T>::iterator::iterator(list *plist)
+list<T>::iterator::iterator(const list<T> *plist)
 {
-    this->plist = plist;
-    cur_node = plist->head;
+    cur_node = plist->beg_node;
 }
 
 template<class T>
@@ -33,31 +32,27 @@ void list<T>::iterator::operator--()
 }
 
 template<class T>
-bool list<T>::iterator::operator==(list::iterator iter)
+bool list<T>::iterator::operator==(list<T>::iterator iter)
 {
-    // TODO: IMPLEMENT ME
-    return false;
+    return this->cur_node == iter.cur_node;
 }
 
 template<class T>
-bool list<T>::iterator::operator!=(list::iterator iter)
+bool list<T>::iterator::operator!=(list<T>::iterator iter)
 {
-    // TODO: IMPLEMENT ME
-    return false;
+    return this->cur_node != iter.cur_node;
 }
 
 template<class T>
-list<T>::reverse_iterator::reverse_iterator(list *plist)
+list<T>::reverse_iterator::reverse_iterator(const list<T> *plist)
 {
-    this->plist = plist;
-    cur_node = plist->tail;
+    cur_node = plist->beg_node->next;
 }
 
 template<class T>
 T list<T>::reverse_iterator::operator*()
 {
-    // TODO: IMPLEMENT ME
-    return nullptr;
+    return this->cur_node->item;
 }
 
 template<class T>
@@ -73,32 +68,44 @@ void list<T>::reverse_iterator::operator--()
 }
 
 template<class T>
-bool list<T>::reverse_iterator::operator==(list::reverse_iterator iter)
+bool list<T>::reverse_iterator::operator==(list<T>::reverse_iterator iter)
 {
-    // TODO: IMPLEMENT ME
-    return false;
+    return this->cur_node == iter.cur_node;
 }
 
 template<class T>
-bool list<T>::reverse_iterator::operator!=(list::reverse_iterator iter)
+bool list<T>::reverse_iterator::operator!=(list<T>::reverse_iterator iter)
 {
-    // TODO: IMPLEMENT ME
-    return false;
+    return this->cur_node != iter.cur_node;
 }
 
 template<class T>
-list<T>::list() = default;
+list<T>::list()
+{
+    this->size = 0;
+    this->beg_node = nullptr;
+}
 
 template<class T>
-list<T>::list(const list &old_list)
+list<T>::list(const list<T> &old_list)
 {
-    // TODO: IMPLEMENT ME
+    const list<T> *test = &old_list;
+    list<T>::iterator iter = list<T>::iterator(&old_list);
+
+    this->size = 0;
+    this->beg_node = nullptr;
+
+    for (unsigned int i = 0; i < old_list.size; i++)
+    {
+        this->push(*iter);
+        ++iter;
+    }
 }
 
 template<class T>
 list<T>::~list()
 {
-    // TODO: IMPLEMENT ME
+    this->free();
 }
 
 template<class T>
@@ -110,7 +117,18 @@ unsigned int list<T>::get_size()
 template<class T>
 void list<T>::free()
 {
-    // TODO: IMPLEMENT ME
+    node *cur_node = this->beg_node;
+    node *next_node = this->beg_node->next;
+
+    while (this->size > 0)
+    {
+        delete cur_node;
+        cur_node = next_node;
+        next_node = next_node->next;
+        this->size--;
+    }
+
+    this->beg_node = nullptr;
 }
 
 template<class T>
@@ -122,35 +140,132 @@ bool list<T>::is_empty()
 template<class T>
 bool list<T>::contains(T item)
 {
-    // TODO: IMPLEMENT ME
-    return false;
+    try
+    {
+        this->get_idx(item);
+    }
+    catch (...) // TODO: make exceptions static
+    {
+        return false;
+    }
+    return true;
+}
+
+template<class T>
+typename list<T>::node *list<T>::get_node_by_idx(unsigned int idx) {
+    unsigned int counter = 0;
+    node *cur_node = this->beg_node;
+
+    if (idx > this->size)
+    {
+        throw std::runtime_error("out of list bounds"); // TODO: make exceptions static
+    }
+
+    while (counter != idx)
+    {
+        cur_node = cur_node->next;
+        counter++;
+    }
+
+    return cur_node;
 }
 
 template<class T>
 T list<T>::get_element_by_idx(unsigned int idx)
 {
-    // TODO: IMPLEMENT ME
-    return nullptr;
+    return get_node_by_idx(idx)->item;
 }
 
 template<class T>
 unsigned int list<T>::get_idx(T item)
 {
-    // TODO: IMPLEMENT ME
-    return 0;
+    unsigned int counter = 0;
+    list::iterator iter_beg = this->begin();
+    list::iterator iter = this->begin();
+
+    do
+    {
+        if (*iter == item)
+        {
+            return counter;
+        }
+        ++iter;
+        ++counter;
+    }
+    while (iter != iter_beg);
+
+    throw std::runtime_error("item not found"); // TODO: make exceptions static
 }
 
 template<class T>
 void list<T>::push(T item)
 {
-    // TODO: IMPLEMENT ME
+    node *new_node = new node(item);
+
+    switch (this->size)
+    {
+        case 0:
+        {
+            this->beg_node = new_node;
+            this->beg_node->next = new_node;
+            this->beg_node->previous = new_node;
+            break;
+        }
+        case 1:
+        {
+            new_node->next = this->beg_node;
+            new_node->previous = this->beg_node;
+            this->beg_node->previous = new_node;
+            this->beg_node->next = new_node;
+            break;
+        }
+        default:
+        {
+            new_node->next = this->beg_node;
+            new_node->previous = this->beg_node->previous;
+            this->beg_node->previous->next = new_node;
+            this->beg_node->previous = new_node;
+            break;
+        }
+    }
+
+    this->size++;
 }
 
 template<class T>
-bool list<T>::push(T item, unsigned int idx)
+void list<T>::push(T item, unsigned int idx)
 {
-    // TODO: IMPLEMENT ME
-    return false;
+    node *new_node;
+    node *prev_node;
+    node *next_node;
+
+    if (idx > this->size)
+    {
+        throw std::runtime_error("out of list bounds"); // TODO: make exceptions static
+    }
+
+    if (idx == this->size)
+    {
+        this->push(item);
+        return;
+    }
+
+    new_node = new node(item);
+    prev_node = this->get_node_by_idx(idx);
+    next_node = prev_node->next;
+
+    new_node->previous = prev_node;
+    new_node->next = next_node;
+
+    prev_node->next = new_node;
+    next_node->previous = new_node;
+
+    if (idx == 0)
+    {
+        this->beg_node = prev_node->next;
+    }
+
+    this->size++;
 }
 
 template<class T>
@@ -170,29 +285,31 @@ bool list<T>::remove(unsigned int idx)
 template<class T>
 typename list<T>::iterator list<T>::begin()
 {
-    // TODO: IMPLEMENT ME
-    return list::iterator(nullptr);
+    return list::iterator(this);
 }
 
 template<class T>
 typename list<T>::reverse_iterator list<T>::rbegin()
 {
-    // TODO: IMPLEMENT ME
-    return list::reverse_iterator(nullptr);
+    return list::reverse_iterator(this);
 }
 
 template<class T>
 typename list<T>::iterator list<T>::end()
 {
-    // TODO: IMPLEMENT ME
-    return list::iterator(nullptr);
+    // TODO: postfix decrement
+    list<T>::iterator iter = list<T>::iterator(this);
+    --iter;
+    return iter;
 }
 
 template<class T>
 typename list<T>::reverse_iterator list<T>::rend()
 {
-    // TODO: IMPLEMENT ME
-    return list::reverse_iterator(nullptr);
+    // TODO: postfix increment
+    list<T>::reverse_iterator iter = list<T>::reverse_iterator(this);
+    ++iter;
+    return iter;
 }
 
 template<class T>
@@ -201,3 +318,5 @@ std::ostream &operator<<(std::ostream &os, const list<T> &list)
     // TODO: IMPLEMENT ME
     return os;
 }
+
+template class list<int>;
