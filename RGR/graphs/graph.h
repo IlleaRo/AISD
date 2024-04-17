1cf4ec3799e5a19be14b78284968a65b1ca5a910
Anton Kharin <aro.kharin@gmail.com>
2024-04-17 21:17:52 +0700

    Переделал структуру

--- RGR/graphs/graph.h
+++ RGR/graphs/graph.h
@@ -1,7 +1,10 @@
-#ifndef GRAPH_H
-#define GRAPH_H
+#ifndef RGR_GRAPH_H
+#define RGR_GRAPH_H
 
 #include <iostream>
+
+#include "vertex.h"
+#include "edge.h"
 
 typedef enum {
     directed,
@@ -13,35 +16,19 @@ typedef enum {
     M
 } graph_form_e;
 
-template <typename T>
+template <class V_NAME_T, class V_DATA_T, class E_WEIGHT_T, class E_DATA_T>
 class graph {
 protected:
     unsigned long num_of_vertex; // Число вершин
     unsigned long num_of_edges; // Число ребер
 
-    class edge {
-        vertex v1; // Дескриптор вершины, из которой исходит ребро.
-        vertex v2; // Дескриптор вершины, в которую входит ребро.
-
-        unsigned long weight; // Вес ребра
-        T data; // Данные, связанные с ребром
-    };
-
 public:
-    class vertex {
-    protected:
-        unsigned long index; // Индекс вершины в структуре графа или -1 (скрытое поле)
-    public:
-        std::string name; // Имя вершины?
-        T data; // Хранимые данные
-    };
-
     // Cоздает пустой L - граф с нулевым числом вершин и ребер
     graph();
 
-    //graph(unsigned long num_of_vertex, graph_type_e type, graph_form_e form);
+    graph(unsigned long num_of_vertex, graph_type_e type, graph_form_e form);
 
-    //graph(unsigned long num_of_vertex, unsigned long num_of_edges, graph_type_e type, graph_form_e form);
+    graph(unsigned long num_of_vertex, unsigned long num_of_edges, graph_type_e type, graph_form_e form);
 
     // Число вершин в графе
     [[nodiscard]] unsigned long get_num_of_vertex() const;
@@ -53,11 +40,11 @@ public:
     graph_type_e get_type();
 
     // Добавляет вершину к графу и возвращает адрес дескриптора вновь созданной вершины
-    vertex *insert_vertex();
+    vertex<V_NAME_T, V_DATA_T> *insert_vertex();
 
-    // Удаляет вершину из графа, заданную адресом дескриптора v,
-    bool remove_vertex(vertex *);
+    // Удаляет вершину из графа, заданную адресом дескриптора,
+    bool remove_vertex(vertex<V_NAME_T, V_DATA_T> * vertex_ptr);
 
 };
 
-#endif //GRAPH_H
+#endif //RGR_GRAPH_H
