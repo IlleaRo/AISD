a85658c4ef8c4c6ab6cb2c14fdef77cae7618ebd
Anton Kharin <aro.kharin@gmail.com>
2024-04-19 12:32:13 +0700

    Продолжаем РГР

--- RGR/graphs/vertex.h
+++ RGR/graphs/vertex.h
@@ -6,7 +6,9 @@ class vertex {
 protected:
     unsigned long index; // Индекс вершины в структуре графа или -1 (скрытое поле)
 public:
-    NAME_T name; // Имя вершины?
+    vertex() : index(0){};
+
+    NAME_T name; // Имя вершины
     DATA_T data; // Хранимые данные
 };
 
