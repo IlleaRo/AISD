61b38d9c11f769f9ca5b6e22522810156df1116f
Anton Kharin <aro.kharin@gmail.com>
2024-04-26 20:37:19 +0700

    Ориентированный L-граф, итератор по вершинам и доработка удаления

--- RGR/main.cpp
+++ RGR/main.cpp
@@ -16,5 +16,21 @@ int main() {
     pretty_graph.remove_vertex(ptr_2);
 
     std::cout<<pretty_graph<<std::endl;
+
+    graph<vertex<std::string, int>, edge<vertex<std::string, int>, double, int>> directed_graph(5, DIRECTED, L);
+    vertex<std::string, int> *d_vertex_1 = directed_graph.insert_vertex("d_vertex_1");
+    vertex<std::string, int> *d_vertex_2 = directed_graph.insert_vertex("d_vertex_2");
+
+    directed_graph.insert_edge(d_vertex_1, d_vertex_2);
+    directed_graph.insert_edge(d_vertex_2, d_vertex_1);
+
+    directed_graph.remove_edge(d_vertex_2, d_vertex_1);
+    std::cout<<directed_graph<<std::endl;
+
+
+    for (vertex_iterator<vertex<std::string, int>> it = directed_graph.begin(); it != directed_graph.end(); ++it) {
+        std::cout<<(*it).get_index()<<std::endl;
+    }
+
     return 0;
 }
