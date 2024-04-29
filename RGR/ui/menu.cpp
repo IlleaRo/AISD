239fdd7573f91c911428bca578dd257e8f05e14c
FeckingPotato <62855317+FeckingPotato@users.noreply.github.com>
2024-04-29 16:14:28 +0700

    приколы с типами

--- RGR/ui/menu.cpp
+++ RGR/ui/menu.cpp
@@ -1,79 +1,66 @@
 #include "menu.h"
 
 // 1.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_type(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_get_type(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_get_type
 }
 
 // 2.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_form(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_get_form(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_get_form
 }
 
 // 3.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_vertices_count(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_get_vertices_count(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_get_vertices_count
 }
 
 // 4.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_edges_count(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_get_edges_count(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_get_edges_count
 }
 
 // 5.
-template <class VERTEX_T, class EDGE_T>
-void menu_insert_vertex(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_insert_vertex(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_insert_vertex
 }
 
 // 6.
-template <class VERTEX_T, class EDGE_T>
-void menu_insert_edge(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_insert_edge(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_insert_edge
 }
 
 // 7.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_vertex(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_get_vertex(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_get_vertex
 }
 
 // 8.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_edge(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_get_edge(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_get_edge
 }
 
 // 9.
-template <class VERTEX_T, class EDGE_T>
-void menu_delete_vertex(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_delete_vertex(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_delete_vertex
 }
 
 // 10.
-template <class VERTEX_T, class EDGE_T>
-void menu_print_graph(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_print_graph(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_print_graph
 }
 
 // 11.
-template <class VERTEX_T, class EDGE_T>
-void menu_clear_graph(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_clear_graph(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_clear_graph
 }
 
 // 12.
-template <class VERTEX_T, class EDGE_T>
-void menu_control_iterator(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_control_iterator(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_control_iterator
 }
 
 // 13.
-template <class VERTEX_T, class EDGE_T>
-void menu_tasks(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights) {
+void menu_tasks(example_graph &pretty_graph, bool use_weights) {
     // TODO: implement menu_tasks
 }
--- RGR/ui/menu.h
+++ RGR/ui/menu.h
@@ -2,56 +2,45 @@
 #define RGR_MENU_H
 #include "../graphs/graph.h"
 
+typedef graph<vertex<std::string, int>, edge<vertex<std::string, int>, double, int>> example_graph;
+
 // 1.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_type(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_get_type(example_graph &pretty_graph, bool use_weights);
 
 // 2.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_form(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_get_form(example_graph &pretty_graph, bool use_weights);
 
 // 3.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_vertices_count(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_get_vertices_count(example_graph &pretty_graph, bool use_weights);
 
 // 4.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_edges_count(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_get_edges_count(example_graph &pretty_graph, bool use_weights);
 
 // 5.
-template <class VERTEX_T, class EDGE_T>
-void menu_insert_vertex(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_insert_vertex(example_graph &pretty_graph, bool use_weights);
 
 // 6.
-template <class VERTEX_T, class EDGE_T>
-void menu_insert_edge(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_insert_edge(example_graph &pretty_graph, bool use_weights);
 
 // 7.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_vertex(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_get_vertex(example_graph &pretty_graph, bool use_weights);
 
 // 8.
-template <class VERTEX_T, class EDGE_T>
-void menu_get_edge(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_get_edge(example_graph &pretty_graph, bool use_weights);
 
 // 9.
-template <class VERTEX_T, class EDGE_T>
-void menu_delete_vertex(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_delete_vertex(example_graph &pretty_graph, bool use_weights);
 
 // 10.
-template <class VERTEX_T, class EDGE_T>
-void menu_print_graph(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_print_graph(example_graph &pretty_graph, bool use_weights);
 
 // 11.
-template <class VERTEX_T, class EDGE_T>
-void menu_clear_graph(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_clear_graph(example_graph &pretty_graph, bool use_weights);
 
 // 12.
-template <class VERTEX_T, class EDGE_T>
-void menu_control_iterator(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_control_iterator(example_graph &pretty_graph, bool use_weights);
 
 // 13.
-template <class VERTEX_T, class EDGE_T>
-void menu_tasks(graph<VERTEX_T, EDGE_T> &pretty_graph, bool use_weights);
+void menu_tasks(example_graph &pretty_graph, bool use_weights);
 
 #endif //RGR_MENU_H
